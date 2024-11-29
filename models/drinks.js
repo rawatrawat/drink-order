20fec0d07076fc14a4e3aee668c0443711a9267f
rawatrawat <167859881+rawatrawat@users.noreply.github.com>
2024-11-29 19:46:33 +0800

    Delete models directory

--- models/drinks.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const drinkSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-});
-
-module.exports = mongoose.model('Drink', drinkSchema);
